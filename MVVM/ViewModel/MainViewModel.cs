375bc16cd8537356c94c1098c229eb66bfb372a2
Ikemous <barranco.ike@outlook.com>
2021-07-22 01:38:39 -0600

    added comments

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -5,14 +5,16 @@ namespace TicTacToe.MVVM.ViewModel
 {
     class MainViewModel : ObservableObject
     {
+        // Create model and commands for the views
         public RelayCommand MainMenuViewCommand { get; set; }
         public MainMenuViewModel MainMenuVM { get; set; }
         public RelayCommand GameViewCommand { get; set; }
         public GameViewModel GameVM { get; set; }
 
-
+        //Create object to control current views
         private object _currentView;
 
+        //Get and set the private object for current views
         public object CurrentView
         {
             get { return _currentView; }
@@ -24,15 +26,17 @@ namespace TicTacToe.MVVM.ViewModel
         }
         public MainViewModel()
         {
+            //Define the Views
             MainMenuVM = new MainMenuViewModel();
             GameVM = new GameViewModel();
             CurrentView = MainMenuVM;
-
+            //MainViewCommand is sent, set current view
             MainMenuViewCommand = new RelayCommand(theObject =>
             {
                 CurrentView = MainMenuVM;
             });
 
+            //GameViewCommand is sent, set current view
             GameViewCommand = new RelayCommand(theObject =>
             {
                 CurrentView = GameVM;
